be2dd1ba8ba62ae3206f60023867b26a20e1f334
florent <florent.galon@mediprix.fr>
2020-09-15 19:18:40 +0200

    graph

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState('cases');
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -96,7 +97,7 @@ function App() {
         <h3>Live Cases by Country</h3>
         <Table countries={tableData} />
         <h3>Worldwide New Cases</h3>
-        <LineGraph />
+        <LineGraph casesType={casesType} />
       </Card>
     </div>
   );
